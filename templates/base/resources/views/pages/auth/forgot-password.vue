87f1641aa4e81f503db443e64e96bffe19c30da8
Shaurya <shauryakumar002@gmail.com>
2022-12-01 15:15:52 +1100

    fix: fortify status message

--- templates/base/resources/views/pages/auth/forgot-password.vue
+++ templates/base/resources/views/pages/auth/forgot-password.vue
@@ -1,36 +1,40 @@
 <script setup lang="ts">
+import unquote from "@/utilities/unquote";
 import InputText from "@/views/components/input-text.vue";
 
 const form = useForm({
-    method: "POST",
-    url: route("password.email"),
-    fields: {
-        email: "",
-    },
+  method: "POST",
+  url: route("password.email"),
+  fields: {
+    email: "",
+  },
 });
 </script>
 
 <template layout="auth">
-    <form @submit.prevent="form.submit" class="space-y-5">
-        <!-- Email -->
-        <InputText
-            v-model="form.fields.email"
-            :errors="form.errors.email"
-            id="email"
-            label="Email"
-            type="email"
-            :required="true"
-        />
+  <form @submit.prevent="form.submit" class="space-y-5">
+    <!-- Email -->
+    <InputText
+      v-model="form.fields.email"
+      :errors="form.errors.email"
+      id="email"
+      label="Email"
+      type="email"
+      :required="true"
+    />
 
-        <span>{{ useProperty("status") }}</span>
+    <!-- fortify returns status in quotes (e.g. "message sent") -->
+    <span class="text-sm font-medium text-theme-success">{{
+      unquote(useProperty("status").value || "")
+    }}</span>
 
-        <button
-            type="submit"
-            class="button w-full"
-            :class="{ 'loading-absolute': form.processing }"
-            :disabled="form.processing"
-        >
-            Reset Password
-        </button>
-    </form>
+    <button
+      type="submit"
+      class="button w-full"
+      :class="{ 'loading-absolute': form.processing }"
+      :disabled="form.processing"
+    >
+      Reset Password
+    </button>
+  </form>
 </template>
--- templates/base/resources/views/pages/auth/login.vue
+++ templates/base/resources/views/pages/auth/login.vue
@@ -1,74 +1,78 @@
 <script setup lang="ts">
+import unquote from "@/utilities/unquote";
+
 import InputText from "@/views/components/input-text.vue";
 import { RouterLink } from "@hybridly/vue";
 
 const form = useForm({
-    method: "POST",
-    url: route("login"),
-    fields: {
-        email: "",
-        password: "",
-        remember: true,
-    },
+  method: "POST",
+  url: route("login"),
+  fields: {
+    email: "",
+    password: "",
+    remember: true,
+  },
 });
 </script>
 
 <template layout="auth">
-    <span>{{ useProperty("status") }}</span>
-    <form @submit.prevent="form.submit" class="space-y-5">
-        <!-- Email -->
-        <InputText
-            v-model="form.fields.email"
-            :errors="form.errors.email"
-            id="email"
-            label="Email"
-            type="email"
-            :required="true"
-        />
+  <!-- fortify returns status in quotes (e.g. "message sent") -->
+  <span class="text-sm font-medium text-theme-success">{{
+    unquote(useProperty("status").value || "")
+  }}</span>
 
-        <!-- Password -->
-        <InputText
-            v-model="form.fields.password"
-            :errors="form.errors.password"
-            id="password"
-            label="Password"
-            type="password"
-            :required="true"
-        />
+  <form @submit.prevent="form.submit" class="space-y-5">
+    <!-- Email -->
+    <InputText
+      v-model="form.fields.email"
+      :errors="form.errors.email"
+      id="email"
+      label="Email"
+      type="email"
+      :required="true"
+    />
 
-        <!-- Remember me -->
-        <div class="flex items-center space-x-2">
-            <input
-                v-model="form.fields.remember"
-                class="checkbox"
-                type="checkbox"
-                name="remember"
-                id="remember"
-            />
-            <label class="text-sm text-theme-muted" for="remember"
-                >Remember me</label
-            >
-        </div>
+    <!-- Password -->
+    <InputText
+      v-model="form.fields.password"
+      :errors="form.errors.password"
+      id="password"
+      label="Password"
+      type="password"
+      :required="true"
+    />
 
-        <div
-            class="flex flex-col items-start justify-between space-x-0 space-y-2 md:flex-row md:items-center md:space-x-4 md:space-y-0"
-        >
-            <RouterLink :href="route('register')" class="button-link">
-                Register account
-            </RouterLink>
+    <!-- Remember me -->
+    <div class="flex items-center space-x-2">
+      <input
+        v-model="form.fields.remember"
+        class="checkbox"
+        type="checkbox"
+        name="remember"
+        id="remember"
+      />
+      <label class="text-sm text-theme-muted" for="remember">Remember me</label>
+    </div>
 
-            <RouterLink :href="route('password.request')" class="button-link">
-                Forgot your password?
-            </RouterLink>
+    <div
+      class="flex flex-col items-start justify-between space-x-0 space-y-2 md:flex-row md:items-center md:space-x-4 md:space-y-0"
+    >
+      <RouterLink :href="route('register')" class="button-link">
+        Register account
+      </RouterLink>
 
-            <button
-                type="submit"
-                class="button w-full py-2 md:w-max"
-                :class="{ loading: form.processing }"
-                :disabled="form.processing"
-            >
-                Sign in
-            </button>
-        </div>
-    </form>
+      <RouterLink :href="route('password.request')" class="button-link">
+        Forgot your password?
+      </RouterLink>
+
+      <button
+        type="submit"
+        class="button w-full py-2 md:w-max"
+        :class="{ loading: form.processing }"
+        :disabled="form.processing"
+      >
+        Sign in
+      </button>
+    </div>
+  </form>
 </template>

4e6ed15ce8508a8bcffd81b23cf05241d5e936a5
Shaurya <shauryakumar002@gmail.com>
2022-12-01 16:29:51 +1100

    added placeholder prop in input-text component

--- templates/base/resources/views/components/input-text.vue
+++ templates/base/resources/views/components/input-text.vue
@@ -1,33 +1,34 @@
 <script setup lang="ts">
 defineProps<{
-    id?: string;
-    label?: string;
-    name?: string;
-    placeholder?: string;
-    type?: string;
-    required?: boolean;
-    modelValue?: string;
-    errors?: string;
+  id?: string;
+  label?: string;
+  name?: string;
+  placeholder?: string;
+  type?: string;
+  required?: boolean;
+  modelValue?: string;
+  errors?: string;
 }>();
 </script>
 
 <template>
-    <div>
-        <label :for="id" class="text-sm text-theme-muted">{{ label }}</label>
-        <input
-            v-on:input="
-                $emit(
-                    'update:modelValue',
-                    ($event?.target as HTMLTextAreaElement)?.value
-                )
-            "
-            :name="name"
-            :id="id"
-            class="input"
-            :type="type || 'text'"
-            :required="required ?? undefined"
-            :value="modelValue"
-        />
-        <span v-if="errors" v-text="errors" class="text-sm text-theme-danger" />
-    </div>
+  <div>
+    <label :for="id" class="text-sm text-theme-muted">{{ label }}</label>
+    <input
+      v-on:input="
+        $emit(
+          'update:modelValue',
+          ($event?.target as HTMLTextAreaElement)?.value
+        )
+      "
+      :name="name"
+      :id="id"
+      class="input"
+      :type="type || 'text'"
+      :required="required ?? undefined"
+      :placeholder="placeholder"
+      :value="modelValue"
+    />
+    <span v-if="errors" v-text="errors" class="text-sm text-theme-danger" />
+  </div>
 </template>
